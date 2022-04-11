939cbb5080b67a29031f34faeb3815a111d39639
PhasitWo <vrzoearth@gmail.com>
2022-04-11 14:23:24 +0700

    DrawImage method.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,6 +3,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
@@ -10,11 +13,15 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenHeight = 720;
     final int FPS = 30;
     private Thread gameThread; // so gameThread will stay until GamePanel instance is disposed
+    private Stage[] stageList;
+    private Stage currentStage;
     
-    public GamePanel() {
+    public GamePanel(Stage[] stageList) {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.green);
         this.setDoubleBuffered(true);
+        this.stageList = stageList;
+        this.currentStage = stageList[0];
        
     }
 
@@ -56,7 +63,38 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g); // paint default component
         Graphics2D g2 = (Graphics2D) g; // downcast to Graphics2D and add our own stuff
         // paint game component
+        ArrayList<String> paths = new ArrayList<>();
+        paths.add("pictures/RSI Divergent.png");
+        paths.add("pictures/default_error.png");
+        paths.add("pictures/default_error.png");
+        paths.add("pictures/default_error.png");
+        this.drawImage(g2, paths);
         
-        
+    }
+    
+    public void drawImage(Graphics2D g, ArrayList<String> imagePath) {
+        BufferedImage bi = null;
+        ArrayList<BufferedImage> imageArr = new ArrayList<>();
+        for (String path : imagePath) {
+            try {
+                imageArr.add(ImageIO.read(getClass().getResourceAsStream(path)));
+            } catch (Exception ex) {
+                try {
+                    imageArr.add(ImageIO.read(getClass().getResourceAsStream("pictures/default_error.png")));
+                } catch (Exception e) {
+                }
+            }
+        }
+        int WIDTH = 200;
+        int HEIGHT = 200;
+        int INTERVAL = 10;
+        int imgCnt = imageArr.size();
+        // find total width of all images (include interval)
+        int totalWidth = (WIDTH*imgCnt) + INTERVAL *(imgCnt - 1);
+        int startPoint = 640 - totalWidth/2;
+        // display image
+        for (int i = 0; i < imgCnt; i++) {
+            g.drawImage(imageArr.get(i), startPoint + (i*(WIDTH+INTERVAL)), 40, WIDTH, HEIGHT, null);
+        }
     }
 }
